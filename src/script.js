1f9326f80ffa27b03ef7774ab9f28a7e85406aa8
Gérald Grévrend <gerald+code@grevrend.com>
2026-06-10 07:51:39 +0200

    Correction du bug random

--- src/script.js
+++ src/script.js
@@ -521,23 +521,26 @@ console.log(filesToTest);
   // ==================== Écran 3: Quiz ====================
   
   const getRandomQuestions = (count) => {
-    //for (let i = [...quizData.questions].length - 1; i > 0; i--) {
-    //	const j = Math.floor(Math.random() * (i + 1));
-    //	[[...quizData.questions][i], [...quizData.questions][j]] = [[...quizData.questions][j], [...quizData.questions][i]];
-  	//}
-  	//return [...quizData.questions].slice(0, count);
+    let temp = [...quizData.questions]
+    for (let i = temp.length - 1; i > 0; i--) {
+    	var j = Math.floor(Math.random() * (i + 1));
+    	[temp[i], temp[j]] = [temp[j], temp[i]];
+  	}
+  	return temp.slice(0, count);
     //ancienne méthode
-    const shuffled = [...quizData.questions].sort(() => 0.5 - Math.random());
-    return shuffled.slice(0, count);
+    //const shuffled = [...quizData.questions].sort(() => 0.5 - Math.random());
+    //return shuffled.slice(0, count);
   };
 
   const shuffleArray = (array) => {
-    return [...array].sort(() => 0.5 - Math.random());
-    //for (let i = [...array].length - 1; i > 0; i--) {
-    //	const j = Math.floor(Math.random() * (i + 1));
-    //	[[...array][i], [...array][j]] = [[...array][j], [...array][i]];
-  	//}
-  	//return [...array];
+    //return [...array].sort(() => 0.5 - Math.random());
+    //let temp = [...array].sort(() => 0.5 - Math.random());
+    let temp = [...array];
+    for (let i = temp.length - 1; i > 0; i--) {
+    	var j = Math.floor(Math.random() * (i + 1));
+    	[temp[i], temp[j]] = [temp[j], temp[i]];
+  	}
+  	return temp;
     //ancienne méthode : return [...array].sort(() => 0.5 - Math.random());
   };
 
